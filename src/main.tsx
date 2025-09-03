c2ccd4ec6a8f353c249d9f237a1c41269f07dec1
Antonio Pérez Sosa <antpersos@gmail.com>
2025-09-03 17:12:16 +0200

    Add vans page, route and link

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import { Link } from "react-router-dom";
+import Vans from "./pages/Vans";
 
 // eslint-disable-next-line react-refresh/only-export-components
 function App() {
@@ -13,12 +14,13 @@ function App() {
         <Link to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
-          <Link to="/">Vans</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
